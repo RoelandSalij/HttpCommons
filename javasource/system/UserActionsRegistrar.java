eaf03ebf072eef7d2a28905be4532e2fd69c2e0a
Robert van 't Hof <robert.van.thof@mendix.com>
2018-05-07 09:25:10 +0200

    Added first test microflows for GetHeader and GetCookie

--- javasource/system/UserActionsRegistrar.java
+++ javasource/system/UserActionsRegistrar.java
@@ -12,6 +12,15 @@ public class UserActionsRegistrar
   public void registerActions(IActionRegistrator registrator)
   {
     registrator.bundleComponentLoaded();
+    registrator.registerUserAction(httpcommons.actions.GetCookie.class);
+    registrator.registerUserAction(httpcommons.actions.GetHeader.class);
+    registrator.registerUserAction(httpcommons.actions.GetIP.class);
+    registrator.registerUserAction(httpcommons.actions.GetRequestString.class);
+    registrator.registerUserAction(httpcommons.actions.GetResourcePath.class);
+    registrator.registerUserAction(httpcommons.actions.SetContentType.class);
+    registrator.registerUserAction(httpcommons.actions.SetCookie.class);
+    registrator.registerUserAction(httpcommons.actions.SetHeader.class);
+    registrator.registerUserAction(httpcommons.actions.SetStatus.class);
     registrator.registerUserAction(system.actions.VerifyPassword.class);
   }
 }
